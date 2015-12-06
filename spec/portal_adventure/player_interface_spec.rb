285f56d0872f1bf47bc4b5d89bb813dfc61b5e41
John Wilger <johnwilger@gmail.com>
2015-12-06 13:44:19 -0800

    Removes :focus accidentally left in spec

--- spec/portal_adventure/player_interface_spec.rb
+++ spec/portal_adventure/player_interface_spec.rb
@@ -8,7 +8,7 @@ RSpec.describe PortalAdventure::PlayerInterface do
   let(:input_stream) { instance_double(IO) }
   let(:output_stream) { instance_double(IO, puts: nil) }
 
-  describe '#handle_location_description', :focus do
+  describe '#handle_location_description' do
     it 'sends the location description to the output stream' do
       subject.handle_location_description 'This is a room.'
       expect(output_stream).to have_received(:puts).with('This is a room.')
