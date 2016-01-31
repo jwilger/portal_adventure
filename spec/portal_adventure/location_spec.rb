b41face51b93cb792ce95c01dea9aa3c8d9f927d
John Wilger <johnwilger@gmail.com>
2016-01-31 11:19:58 -0800

    Clean slate to rethink interaction of interface with universe

--- spec/portal_adventure/location_spec.rb
+++
@@ -1,7 +0,0 @@
-require 'portal_adventure/location'
-
-RSpec.describe PortalAdventure::Location do
-  it 'has a description' do
-    expect(subject.description).to eq "You are in a room with a description."
-  end
-end
